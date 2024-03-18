cdfd8c98c24b4ba5c5e3c98593dae82259a383bc
Alexander Chernykh <avchernykh@mtsbank.ru>
2024-03-18 10:21:19 +0300

    feat: added draft version of advantages section of main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Advantages from "./components/advantages/Advantages";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Advantages />
+    </main>
+  );
 }
