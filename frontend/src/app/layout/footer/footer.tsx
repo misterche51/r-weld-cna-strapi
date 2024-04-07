c39b59e2e0d84d62e3fc8b23e2033eb1a02d18c7
Alexander Chernykh <avchernykh@mtsbank.ru>
2024-04-07 12:17:27 +0300

    refactor: improved footer adaptivity

--- frontend/src/app/layout/footer/footer.tsx
+++ frontend/src/app/layout/footer/footer.tsx
@@ -32,6 +32,60 @@ const SITE_MAP_DATA = [
 
 const CONTACTS_DATA = ["8 800 900-10-10", "info@r-weld.ru"];
 
+const Logo = () => (
+  <div className={styles.logo}>
+    <Link href={"/"}>
+      <Image
+        className={styles.logo__img}
+        src="/white-logo.svg"
+        alt="Логотип R-WELD"
+        width={148}
+        height={78}
+        priority
+      />
+    </Link>
+  </div>
+);
+
+const Description = () => (
+  <p className={styles.description}>
+    Российский производитель и поставщик горелок для MIG и TIG сварки
+  </p>
+);
+
+const SiteMap = () => (
+  <div className={styles.site_map}>
+    <p className={styles.site_map__heading}>Разделы</p>
+    <ul className={styles.site_map__list}>
+      {SITE_MAP_DATA.map(({ label, target }, i) => (
+        <li className={styles.site_map__item} key={i}>
+          <Link href={`/${target}`}>{label}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+const Contacts = () => (
+  <div className={styles.contacts}>
+    <p className={styles.contacts__heading}>Контакты</p>
+    <ul className={styles.contacts__list}>
+      {CONTACTS_DATA.map((label, i) => (
+        <li className={styles.contacts__item} key={i}>
+          {label}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+const Address = () => (
+  <p className={styles.address}>
+    ООО «Р-ВЕЛД» 390525 Рязанская обл., Рязанский муниципальный район,
+    с. Поляны, ул. Новая, строение 15
+  </p>
+);
+
 export default function Footer({ children }: PropsWithChildren) {
   return (
     <footer className={styles.wrapper}>
@@ -39,50 +93,19 @@ export default function Footer({ children }: PropsWithChildren) {
         <div className={styles.inner}>
           <div className={styles.info_box}>
             <div className={styles.company_info}>
-              <div className={styles.logo}>
-                <Link href={"/"}>
-                  <Image
-                    className={styles.logo}
-                    src="/white-logo.svg"
-                    alt="Логотип R-WELD"
-                    width={148}
-                    height={78}
-                    priority
-                  />
-                </Link>
-              </div>
-              <p className={styles.description}>
-                Российский производитель и поставщик горелок для MIG и TIG
-                сварки
-              </p>
+              <Logo />
+              <Description />
             </div>
-
-            <p className={styles.address}>
-              ООО «Р-ВЕЛД» 390525 Рязанская обл., Рязанский муниципальный район,
-              с. Поляны, ул. Новая, строение 15
-            </p>
+            <div className={styles["address_wrapper--desk"]}>
+              <Address />
+            </div>
           </div>
           <div className={styles.links_box}>
-            <div className={styles.site_map}>
-              <p className={styles.site_map__heading}>Разделы</p>
-              <ul className={styles.site_map__list}>
-                {SITE_MAP_DATA.map(({ label, target }, i) => (
-                  <li className={styles.site_map__item} key={i}>
-                    <Link href={`/${target}`}>{label}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className={styles.contacts}>
-              <p className={styles.contacts__heading}>Контакты</p>
-              <ul className={styles.contacts__list}>
-                {CONTACTS_DATA.map((label, i) => (
-                  <li className={styles.contacts__item} key={i}>
-                    {label}
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <SiteMap />
+            <Contacts />
+          </div>
+          <div className={styles["address_wrapper--mob"]}>
+            <Address />
           </div>
         </div>
       </Container>
