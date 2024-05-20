1e4594bfda6ab169c629e57965f418fa317578c2
Alexander Chernykh <avchernykh@mtsbank.ru>
2024-05-20 15:36:34 +0300

    feat: relized planets and others pages in chemistry section:

---
+++ frontend/src/app/production/chemistry/others/page.tsx
@@ -0,0 +1,29 @@
+// "use client";
+
+import styles from "./styles.module.css";
+import Layout from "@/app/layout/layout";
+import Container from "@/app/layout/container/container";
+import Heading from "@/app/shared/heading/heading";
+import ContactForm from "@/app/shared/contactForm/contactForm";
+import { CHEMISTRY_DB } from "@/api/db";
+import Description from "@/app/shared/description/description";
+
+const Page = () => {
+  const { label, description } = CHEMISTRY_DB.others;
+  return (
+    <Layout>
+      <div className={styles.wrapper}>
+        <Container>
+          <div className={styles.inner}>
+            <Heading rank={2} text={label} withUnderline={true} />
+            {description && <Description description={description} />}
+            <div className={styles.form}>
+              <ContactForm />
+            </div>
+          </div>
+        </Container>
+      </div>
+    </Layout>
+  );
+};
+export default Page;
---
+++ frontend/src/app/production/chemistry/planers/page.tsx
@@ -0,0 +1,55 @@
+// "use client";
+
+// import { useRouter } from "next/router";
+import Image from "next/image";
+import styles from "./styles.module.css";
+import Layout from "@/app/layout/layout";
+import Container from "@/app/layout/container/container";
+import Heading from "@/app/shared/heading/heading";
+import ContactForm from "@/app/shared/contactForm/contactForm";
+import { CHEMISTRY_DB } from "@/api/db";
+import ImageWrapper from "../components/imageWrapper/imageWrapper";
+import Description from "@/app/shared/description/description";
+
+const Page = () => {
+  const { label, description, images } = CHEMISTRY_DB.planers;
+  const sectionNames = Object.keys(images);
+  return (
+    <Layout>
+      <div className={styles.wrapper}>
+        <Container>
+          <div className={styles.inner}>
+            <Heading rank={2} text={label} withUnderline={true} />
+            {description && <Description description={description} />}
+            {sectionNames.map((name) => {
+              const { title, alt, src } = images[name];
+              return (
+                <ImageWrapper
+                  title={title}
+                  image={
+                    <div
+                      className={`${styles.image} ${styles[`image--${name}`]}`}
+                    >
+                      <Image
+                        className={styles.image__pic}
+                        src={`/planers/${src}`}
+                        alt={alt}
+                        fill
+                        style={{ objectFit: "contain" }}
+                        priority
+                      />
+                    </div>
+                  }
+                />
+              );
+            })}
+            <div className={styles.form}>
+              <ContactForm />
+            </div>
+          </div>
+        </Container>
+      </div>
+    </Layout>
+  );
+};
+export default Page;
