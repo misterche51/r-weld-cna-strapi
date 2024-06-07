531a9f8c572f49451e6aaef35c77b8deee1b1973
Черных Александр Викторович <achernykh@t1.ru>
2024-06-07 20:14:54 +0300

    refactor: improved transition for button component

--- frontend/src/app/shared/button/button.tsx
+++ frontend/src/app/shared/button/button.tsx
@@ -4,9 +4,11 @@ type TButtonProps = {
   text: string;
 };
 export default function Button({ variant = "dark", text }: TButtonProps) {
+  const buttonClassNameByTheme = `${styles[`comp--${variant}`]}`
+
   return (
-    <button className={`${styles[`comp--${variant}`]} ${styles.comp}`}>
-      {text}
+    <button className={`${styles.comp} ${buttonClassNameByTheme}`}>
+      <p className={styles.label}>{text}</p>
     </button>
   );
 }
