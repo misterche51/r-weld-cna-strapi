5a8ea62fb685ae73edc9a070fa8ba98fd80dc5d8
Temkarzn <sl_62@mail.ru>
2024-09-02 16:53:49 +0300

    Update mig_mb_liquid.ts

--- src/api/catalog/torches/classes/mig/categories/mig_mb_liquid.ts
+++ src/api/catalog/torches/classes/mig/categories/mig_mb_liquid.ts
@@ -1,4 +1,52 @@
 export const MIG_MB_LIQUID = {
     label: "",
-    list: [],
+    list: [
+{
+        name: "MB EVO 240D",
+        image: "",
+        co2: "300 A",
+        gas: "270 A",
+        pv: "100%",
+        wire: "0.8 - 1.2 мм",
+      },
+      {
+        name: "MB EVO 401D",
+        image: "",
+        co2: "400 A",
+        gas: "350 A",
+        pv: "100%",
+        wire: "0.8 - 1.2 мм",
+      },
+      {
+        name: "MB EVO 401",
+        image: "",
+        co2: "450 A",
+        gas: "400 A",
+        pv: "100%",
+        wire: "0.8 - 1.2 мм",
+      },
+      {
+        name: "MB EVO 501D",
+        image: "",
+        co2: "500 A",
+        gas: "450 A",
+        pv: "100%",
+        wire: "1.0 - 1.6 мм",
+      },
+      {
+        name: "MB EVO 501",
+        image: "",
+        co2: "550 A",
+        gas: "500 A",
+        pv: "100%",
+        wire: "1.0 - 1.6 мм",
+      },
+      {
+        name: "RF GRIP 45",
+        image: "",
+        co2: "450 A",
+        gas: "400 A",
+        pv: "60%",
+        wire: "1.0 - 2.4 мм",
+      },],
   }
