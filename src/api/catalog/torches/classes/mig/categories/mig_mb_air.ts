63df2271fd9adc381d0186180bf5659ac351da84
Temkarzn <sl_62@mail.ru>
2024-09-02 15:54:18 +0300

    Update mig_mb_air.ts

--- src/api/catalog/torches/classes/mig/categories/mig_mb_air.ts
+++ src/api/catalog/torches/classes/mig/categories/mig_mb_air.ts
@@ -2,20 +2,39 @@ export const MIG_MB_AIR = {
   label: "Ручные горелки MB EVO",
   list: [
  {
-      name: "MB EVO 15",
-      image: "",
-      co2: "180 A",
-      gas: "150 A",
-      pv: "60%",
-      wire: "проба",
- }
-,
- {
-      name: "RF GRIP 25",
-      image: "",
-      co2: "180 A",
-      gas: "150 A",
-      pv: "60%",
-      wire: "фыафафафафафа",}
+        name: "MB EVO 15",
+        image: "",
+        co2: "180 A",
+        gas: "150 A",
+        pv: "60%",
+        wire: "0.6 - 1.0 мм",
+      },
+      {
+        name: "MB EVO 25",
+        image: "",
+        co2: "230 A",
+        gas: "200 A",
+        pv: "60%",
+        wire: "0.8 - 1.2 мм",
+      },
+      {
+        name: "MB EVO 24",
+        image: "",
+        co2: "250 A",
+        gas: "220 A",
+        pv: "60%",
+        wire: "0.8 - 1.2 мм",
+      },
+      {
+        name: "MB EVO 26",
+        image: "",
+        co2: "270 A",
+        gas: "240 A",
+        pv: "60%",
+        wire: "0.8 - 1.2
+          
+          мм",
+      },
+     
 ],
 }
