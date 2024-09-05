d2786dd715f20f6df026338a825b9c70da0ded8f
Temkarzn <sl_62@mail.ru>
2024-09-05 14:27:50 +0300

    Update db.ts

--- src/api/dealers/db.ts
+++ src/api/dealers/db.ts
@@ -166,20 +166,20 @@ export const DEALERS_DB: TRegionItem[] = [
 {
     region: "Ленинградская область",
     list: [
+   {
+        name: "ООО «СКАЙ-РЕСУРС»",
+        phone: "(812) 900-35-85",
+        address: "г. Санкт-Петербург, БЦ Петровско-спасская мануфактура пр. Обуховской обороны 86К, оф. 222",
+        email: "info@sky-resource.ru",
+        site: "www.sky-resource.ru",
+        city: "санкт-петербург",
+      },
       {
         name: "ООО «Сварной»",
         phone: "(812) 635-87-70, 927-14-23",
         address: "г. Санкт-Петербург, 5-Й Предпортовый пр-д, дом № 3, литера А, помещение №172",
         email: "info@svarnoy.ru",
         site: "www.svarnoy.ru",
-        city: "санкт-петербург",
-      },
-      {
-        name: "ООО «СКАЙ-РЕСУРС»",
-        phone: "(812) 900-35-85",
-        address: "г. Санкт-Петербург, БЦ Петровско-спасская мануфактура пр. Обуховской обороны 86К, оф. 222",
-        email: "info@sky-resource.ru",
-        site: "www.sky-resource.ru",
         city: "санкт-петербург",
       },
       {
@@ -345,6 +345,280 @@ export const DEALERS_DB: TRegionItem[] = [
         email: "torus54@mail.ru",
         site: "www.torus.spb.ru",
         city: "новосибирск",
+      },
+      {
+        name: "ООО «ВДС»",
+        phone: "(383) 209-35-60",
+        address: "г. Новосибирск, ул. Писарева, д. 60, пом. 2.6.4",
+        email: "mail@n-weld.ru",
+        site: "www.vdsperm.ru",
+        city: "новосибирск",
+      },
+ {
+        name: "ООО «РСТП»",
+        phone: "(923) 775-14-17",
+        address: "",
+        email: "nsk@rsco.ru",
+        site: "www.rstp.ru",
+        city: "новосибирск",
+      },
+    ],
+  },
+{
+    region: "Омская область",
+    list: [
+      {
+        name: "ООО «АРМИГ»",
+        phone: "(3812) 788-057",
+        address: "г. Омск, ул 10 лет Октября, д. 182, офис 204",
+        email: "omsk@armig.ru",
+        site: "www.armig.ru",
+        city: "омск",
+      },
+    ],
+  },
+{
+    region: "Оренбургска область",
+    list: [
+      {
+        name: "ООО «Криотэк»",
+        phone: "(3532) 44-05-66",
+        address: "г. Оренбург, проезд Автоматики, дом № 5/1",
+        email: "995361@mail.ru",
+        site: "www.криотэк.рф",
+        city: "оренбург",
+      },
+    ],
+  },
+{
+    region: "Пермский край",
+    list: [
+      {
+        name: "ООО «ВДС»",
+        phone: "(342) 213-61-36, 213-61-00, 213-61-01",
+        address: "г. Пермь, ул. Докучаева, дом 33, офис 308",
+        email: "vds@vdsperm.ru",
+        site: "www.vdsperm.ru",
+        city: "пермь",
+      },
+    ],
+  },
+{
+    region: "Приморский край",
+    list: [
+      {
+        name: "ООО «Эдельвейс»",
+        phone: "(423) 224-29-65, 295-10-08",
+        address: "г. Владивосток, ул. Снеговая, дом № 18а, литер 2",
+        email: "office@svarka-vl.com",
+        site: "www.svarka-vl.com",
+        city: "владивосток",
+      },
+    ],
+  },
+{
+    region: "Республика Башкортостан",
+    list: [
+      {
+        name: "ООО «СВАРСНАБ»",
+        phone: "(987) 040-50-60",
+        address: "Башкортостан респ, м.р-н Уфимский, с Жуково, ул Цветочная, д. 58",
+        email: "info@svarsnab-ufa.ru",
+        site: "www.svarsnab-ufa.ru",
+        city: "башкортостан",
+      },
+    ],
+  },
+{
+    region: "Республика Татарстан",
+    list: [
+      {
+        name: "ООО НПП "РУ-ИНЖИНИРИНГ"",
+        phone: "(8552) 39-98-02",
+        address: "г. Набережные Челны г, Мензелинский тракт, дом № 14, офис 313",
+        email: "mail@ru-drive.com",
+        site: "www.ru-drive.com",
+        city: "татарстан",
+      },
+    ],
+  },
+{
+    region: "Ростовская область",
+    list: [
+      {
+        name: "ООО «СТЭМ»",
+        phone: "(863) 333-29-33",
+        address: "г. Ростов-на-Дону, ул. Таганрогская, 144Б/18, литер И",
+        email: "stem@stem-rostov.ru",
+        site: "www.stem-rostov.ru",
+        city: "ростов",
+      },
+      {
+        name: "ООО «Радикс-РНД»",
+        phone: "(863) 282-03-23, 236-93-27",
+        address: "г. Ростов-на-Дону, ул. Портовая, 33",
+        email: "order@kontur-svar.ru",
+        site: "www.svarkarnd.ru",
+        city: "ростов",
+      },
+    ],
+  },{
+    region: "Самарская область",
+    list: [
+      {
+        name: "ООО «Волжская промышленная группа»",
+        phone: "(846) 200-42-42",
+        address: "г. Самара, ул.22 партсъезда, 2, оф.5",
+        email: "info@vpg-s.ru",
+        site: "www.vpg-s.ru",
+        city: "самара",
+      },
+{
+        name: "ООО «Энтузиаст-С»",
+        phone: "(846) 374-66-86",
+        address: "г. Самара, ул. Победы, 100",
+        email: "order@kuvalda.ru",
+        site: "www.kuvalda.ru",
+        city: "самара",
+      },
+    ],
+  },
+{
+    region: "Свердловская область",
+    list: [
+      {
+        name: "ООО «ДЕЛЬТАСВАР»",
+        phone: "(343) 384-71-72, 287-41-52",
+        address: "г. Екатеринбург, ул. Завокзальная, д. 29",
+        email: "ekb@deltasvar.ru",
+        site: "www.deltasvar.ru",
+        city: "екатеринбург",
+      },
+ {
+        name: "ООО «ФОКСВЭЛД УРАЛ»",
+        phone: "(343) 247-23-33",
+        address: "г. Екатеринбург, ул Монтажников , д. 24",
+        email: "mail@foxweld-ural.ru",
+        site: "www.foxweld.ru",
+        city: "екатеринбург",
+      },
+{
+        name: "ООО «ОПТИМА ВЕЛД»",
+        phone: "(800) 550-47-18",
+        address: "г. Екатеринбург, Тверитина ул., дом СООР. 34-Б, офис 1",
+        email: "info@vsyasvarka.ru",
+        site: "www.vsyasvarka.ru",
+        city: "екатеринбург",
+      },
+{
+        name: "ООО «ЛЭС "Альма"»",
+        phone: "(343) 361-66-56",
+        address: "г. Каменск-Уральский, ул. Мусоргского, д.9",
+        email: "alma.snab@mail.ru",
+        site: "",
+        city: "екатеринбург",
+      },
+{
+        name: "ООО «Сварка 66»",
+        phone: "(343) 226-08-36",
+        address: "г. Екатеринбург, проезд Промышленный, дом № 11, оф.1",
+        email: ""zakaz@svarka66.ru",
+        site: "www.svarka66.ru",
+        city: "екатеринбург",
+      },
+    ],
+  },
+{
+    region: "Тверская область",
+    list: [
+      {
+        name: "ООО «МТИ»",
+        phone: "(4822) 22-34-50",
+        address: "г. Тверь, ул. Лукина, д. 4, корп. 1",
+        email: "mail@mti.ru.com",
+        site: "",
+        city: "тверь",
+      },
+    ],
+  },
+{
+    region: "Тульская область",
+    list: [
+      {
+        name: "ООО «ИНТЕРСВАРКА»",
+        phone: "(4872) 317-945",
+        address: "г. Тула, Скуратовский п, Шахтерская ул, дом № 5 А",
+        email: "info@intersvar.ru",
+        site: "www.intersvar.ru",
+        city: "Тула",
+      },
+    ],
+  },
+{
+    region: "Тюменская область",
+    list: [
+      {
+        name: "ООО «ЭмаксТехно»",
+        phone: "(3452) 58-96-54, 58-96-45",
+        address: "г. Тюмень, ул. Московский тракт, д. 163 Б",
+        email: "emax@emaxweld.com",
+        site: "www.emaxweld.com",
+        city: "Тюмень",
+      },
+    ],
+  },
+{
+    region: "Хабаровский край",
+    list: [
+      {
+        name: "ООО «Оборудование для резки и сварки»",
+        phone: "(4212) 54-30-60, 54-19-88",
+        address: "г.Хабаровск ул.Световая д. 9А",
+        email: "svarka@svarka.su",
+        site: "www.svarka.su",
+        city: "хабаровск",
+      },
+    ],
+  },
+{
+    region: "Челябинская область",
+    list: [
+      {
+        name: "ООО НПК "СОЕДИНЕНИЕ"",
+        phone: "(351) 216-06-28",
+        address: "г. Челябинск, ул. Молодогвардейцев, дом № 47а, оф.10",
+        email: "info@soedinenie.pro",
+        site: "www.soedinenie.pro",
+        city: "челябинск",
+      },
+    {
+        name: "ООО Научно-производственное предприятие "Сварка-74"",
+        phone: "(804)333-18-18",
+        address: "г. Челябинск, Рылеева ул., дом № 11",
+        email: "info@svarka74.ru",
+        site: "www.svarka74.ru",
+        city: "челябинск",
+      },
+    ],
+  },
+{
+    region: "Чувашская республика / Чувашия",
+    list: [
+      {
+        name: "ООО НПП "ПРОМТЕХНОСВАР-Ч"",
+        phone: "(8352) 49-30-70, 49-30-71, 70-00-74",
+        address: "г. Чебоксары, Машиностроителей пр-д, дом № 1 "У", помещение 3",
+        email: "pts-ch@mail.ru",
+        site: "www.pts21.ru",
+        city: "чебоксары",
+      },
+    {
+        name: "ООО "СВАРЭКС"",
+        phone: "(8352) 45-82-83, 640-690",
+        address: "г. Чебоксары, ул. Коммунальная Слобода, дом № 27",
+        email: "sale@prosvar.ru",
+        site: "www.prosvar.ru",
+        city: "чебоксары",
       },
     ],
   },
