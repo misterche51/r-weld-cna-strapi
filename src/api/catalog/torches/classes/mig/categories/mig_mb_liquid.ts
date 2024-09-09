e276db5715e871e4b6c8355235ce07ef72f3ffce
Temkarzn <sl_62@mail.ru>
2024-09-09 16:29:52 +0300

    Update mig_mb_liquid.ts

--- src/api/catalog/torches/classes/mig/categories/mig_mb_liquid.ts
+++ src/api/catalog/torches/classes/mig/categories/mig_mb_liquid.ts
@@ -1,30 +1,6 @@
 export const MIG_MB_LIQUID = {
     label: "",
     list: [
-{
-        name: "MB EVO 240D",
-        image: "",
-        co2: "300 A",
-        gas: "270 A",
-        pv: "100%",
-        wire: "0.8 - 1.2 мм",
-      },
-      {
-        name: "MB EVO 401D",
-        image: "",
-        co2: "400 A",
-        gas: "350 A",
-        pv: "100%",
-        wire: "0.8 - 1.2 мм",
-      },
-      {
-        name: "MB EVO 401",
-        image: "",
-        co2: "450 A",
-        gas: "400 A",
-        pv: "100%",
-        wire: "0.8 - 1.2 мм",
-      },
       {
         name: "MB EVO 501D",
         image: "",
@@ -33,12 +9,5 @@ export const MIG_MB_LIQUID = {
         pv: "100%",
         wire: "1.0 - 1.6 мм",
       },
-      {
-        name: "MB EVO 501",
-        image: "",
-        co2: "550 A",
-        gas: "500 A",
-        pv: "100%",
-        wire: "1.0 - 1.6 мм",
-      },],
+   ],
   }
