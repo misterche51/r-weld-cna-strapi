711af825e0c4fba4b95037cdfedf1a2b579cca3d
Temkarzn <sl_62@mail.ru>
2024-09-10 17:27:37 +0300

    Update tig_r-weld_air.ts

--- src/api/catalog/torches/classes/tig/categories/tig_r-weld_air.ts
+++ src/api/catalog/torches/classes/tig/categories/tig_r-weld_air.ts
@@ -3,9 +3,38 @@ export const TIG_R_WELD_AIR = {
     list: [{
         name: "RWTIG 9",
         image: "",
-        dc: "",
-        ac: "",
+        dc: "110 A",
+        ac: "80 A",
         pv: "35%",
         wire: "0.5 - 1.6 мм",
-      },],
+        flex: "да"
+      },
+{
+        name: "RWTIG 17",
+        image: "",
+        co2: "140 A",
+        gas: "100 A",
+        pv: "35%",
+        wire: "0.5 - 2.4 мм",
+        flex: "да"
+      },
+{
+        name: "RWTIG 26",
+        image: "",
+        co2: "180 A",
+        gas: "130 A",
+        pv: "35%",
+        wire: "0.5 - 4.0 мм",
+        flex: "да"
+      },
+{
+        name: "RWTIG 200",
+        image: "",
+        co2: "200 A",
+        gas: "140 A",
+        pv: "35%",
+        wire: "1.6 - 3.2 мм",
+        flex: "да"
+      },
+],
   }
