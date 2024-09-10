96f2f579af27ac1d725408d8628fbfb11fdb3066
Temkarzn <sl_62@mail.ru>
2024-09-10 17:31:41 +0300

    Update tig_r-weld_liquid.ts

--- src/api/catalog/torches/classes/tig/categories/tig_r-weld_liquid.ts
+++ src/api/catalog/torches/classes/tig/categories/tig_r-weld_liquid.ts
@@ -1,4 +1,32 @@
 export const TIG_R_WELD_LIQUID = {
     label: "",
-    list: [],
+    list: [
+{
+        name: "RWTIG 18",
+        image: "",
+        co2: "320 A",
+        gas: "230 A",
+        pv: "100%",
+        wire: "0.5 - 4.0 мм",
+        flex: "да"
+      },
+{
+        name: "RWTIG 18 SC",
+        image: "",
+        co2: "400 A",
+        gas: "280 A",
+        pv: "100%",
+        wire: "0.5 - 4.8 мм",
+        flex: "нет"
+      },
+{
+        name: "RWTIG 450 W",
+        image: "",
+        co2: "420 A / 400 A",
+        gas: "300 A / 280 A",
+        pv: "60% / 100%",
+        wire: "1.6 - 4.8 мм",
+        flex: "нет"
+      },
+],
   }
