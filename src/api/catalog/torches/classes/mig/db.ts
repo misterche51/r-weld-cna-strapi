e644aa4f3629a4ed9f3fc4fb51b159a9f7f834ba
Temkarzn <sl_62@mail.ru>
2024-10-21 16:55:35 +0300

    Update db.ts

--- src/api/catalog/torches/classes/mig/db.ts
+++ src/api/catalog/torches/classes/mig/db.ts
@@ -17,7 +17,7 @@ export const TORCHES_MIG_DB:TTorchesClass = {
           absoluteTarget: "/production/hand_torches/mig_rf_air",
           data: MIG_RF_AIR,
           //@ts-expect-error
-          tags: [...MIG_RF_AIR.list.reduce((acc, curr) => {return [...acc, ...curr.name.split(' '), curr.name]}, []).map(tag => tag.toLowerCase().replace(' ', '+')), 'grip+15', 'grip', 'rf+grip']
+          tags: [...MIG_RF_AIR.list.reduce((acc, curr) => {return [...acc, ...curr.name.split(' '), curr.name]}, []).map(tag => tag.toLowerCase().replace(' ', '+')), 'grip+15', 'grip', 'rf+grip', 'rf+25', 'рф+25', 'rf+26', 'рф+26', 'rf+36', 'рф+36', 'rf+45', 'рф+45', 'горелка+rf', 'горелка+рф', 'rf15', 'рф15', 'rf25', 'рф25', 'rf26', 'рф26', 'rf36', 'рф36', 'rf45', 'рф45']
         },
         {
           label: "MB",
@@ -25,7 +25,7 @@ export const TORCHES_MIG_DB:TTorchesClass = {
           absoluteTarget: "/production/hand_torches/mig_mb_air",
           data: MIG_MB_AIR,
           //@ts-expect-error
-          tags: [...MIG_MB_AIR.list.reduce((acc, curr) => {return [...acc, ...curr.name.split(' '), curr.name]}, []).map(tag => tag.toLowerCase().replace(' ', '+'))]
+          tags: [...MIG_MB_AIR.list.reduce((acc, curr) => {return [...acc, ...curr.name.split(' '), curr.name]}, []).map(tag => tag.toLowerCase().replace(' ', '+')), 'mb', 'мб', 'мв', 'mb+25', 'мб+25', 'mb+26', 'мб+26', 'mb+36', 'мб+36', 'mb+15', 'мб+15','mb+evo', 'мб+ево', 'mb+evo+25', 'мб+ево+25', 'mb+evo+26', 'мб+ево+26', 'mb+evo+36', 'мб+ево+36', 'mb+evo+15', 'мб+ево+15', 'мб+эво', 'мб+эво+25', 'мб+эво+26', 'мб+эво+36', 'мб+эво+15', 'горелка+mb+evo', 'горелка+мб+ево', 'горелка+мбево', 'горелка+мб+эво', 'горелка+мбэво', 'горелка+mbevo', 'mb15', 'мб15', 'mb25', 'мб25', 'mb26', 'мб26', 'mb36', 'мб36']
         },
         {
           label: "R-WELD",
