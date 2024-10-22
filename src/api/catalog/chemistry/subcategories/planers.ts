945f199f2eabf306ea386a97040622e1db436d74
Черных Александр Викторович <achernykh@t1.ru>
2024-10-22 21:47:23 +0300

    fix: fixed planers

--- src/api/catalog/chemistry/subcategories/planers.ts
+++ src/api/catalog/chemistry/subcategories/planers.ts
@@ -53,7 +53,8 @@ export const PLANERS_CATALOG_DATA : IPlanersDB = {
       'К16', 'К 16', 'K16', 'K 16',
       'К16Т', 'К 16 Т', 'K16 T', 'K 16T',
       'К20', 'К 20', 'K20', 'K 20',
-      ,'строгач','строгачи','строжка',
+      ,'строгач','строгачи','строжка'
+      // @ts-expect-error
     ].map(tag => tag.toLowerCase().replace(' ', '+'))
   }
 
