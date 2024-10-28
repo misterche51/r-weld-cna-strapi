09d998f3d2c54ca14c3190dd044ebe289b438c0d
Черных Александр Викторович <achernykh@t1.ru>
2024-10-28 22:12:13 +0300

    fix: fixed mailphp

--- public/mail.php
+++ public/mail.php
@@ -1,6 +1,7 @@
 <?php
+    $subject = '=?utf-8?b?'. base64_encode('Заявка с сайта') .'?=';
     $email = $_POST['user_phone'];
     $message = "Пользователь оставил заявку,\nего телефон: $phone";
     $message = wordwrap($message, 70);
-    var_dump(mail('info@r-weld.ru', 'Заявка с сайта', $message));
+    var_dump(mail('info@r-weld.ru', 'Заявка с сайта', $message,  "Content-type: text/html; charset=utf-8"));
 ?>
