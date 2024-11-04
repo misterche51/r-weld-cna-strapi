c948580ff96fcc9c950b8f550e1164e04090c92e
Черных Александр Викторович <achernykh@t1.ru>
2024-11-04 21:29:54 +0300

    fix: fixed deps

--- src/app/layout/container/container.tsx
+++ src/app/layout/container/container.tsx
@@ -5,12 +5,5 @@ import styles from "./container.module.css";
 export default function Container({ children }: PropsWithChildren) {
   return <div className={styles.wrapper}>
     {children}
-    <button className={styles.scroll_btn} onClick={(e) => {
-      e.preventDefault();
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-        })}}></button>
   </div>;
 }
